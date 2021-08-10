1ad63295385bc17ec58cecaaade48f9b51025522
Thiago A <thiago.augusto.alvarenga@gmail.com>
2021-08-10 00:25:19 -0300

    troca de funçoes ajax por fetch

--- js/spotify-web.js
+++ js/spotify-web.js
@@ -1,77 +1,61 @@
+var spotifyApi = new SpotifyWebApi();
+const spotifyIcon = '<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M19.1,10.64C15.23,8.34,8.85,8.13,5.16,9.25A1.12,1.12,0,1,1,4.51,7.1c4.23-1.28,11.28-1,15.73,1.61a1.12,1.12,0,1,1-1.14,1.93ZM19,14a.94.94,0,0,1-1.29.31A15.73,15.73,0,0,0,5.73,13a.94.94,0,0,1-.55-1.79,17.5,17.5,0,0,1,13.48,1.6A.93.93,0,0,1,19,14ZM17.5,17.31a.75.75,0,0,1-1,.25c-2.82-1.72-6.37-2.11-10.55-1.16A.74.74,0,0,1,5,15.84a.75.75,0,0,1,.56-.9c4.57-1,8.49-.59,11.66,1.34A.75.75,0,0,1,17.5,17.31ZM12,0A12,12,0,1,0,24,12,12,12,0,0,0,12,0Z" style="fill:#fff"/></svg>&nbsp';
 const clientId = '';
 const clientSecret = '';
-var token
-var spotifyIcon = '<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M19.1,10.64C15.23,8.34,8.85,8.13,5.16,9.25A1.12,1.12,0,1,1,4.51,7.1c4.23-1.28,11.28-1,15.73,1.61a1.12,1.12,0,1,1-1.14,1.93ZM19,14a.94.94,0,0,1-1.29.31A15.73,15.73,0,0,0,5.73,13a.94.94,0,0,1-.55-1.79,17.5,17.5,0,0,1,13.48,1.6A.93.93,0,0,1,19,14ZM17.5,17.31a.75.75,0,0,1-1,.25c-2.82-1.72-6.37-2.11-10.55-1.16A.74.74,0,0,1,5,15.84a.75.75,0,0,1,.56-.9c4.57-1,8.49-.59,11.66,1.34A.75.75,0,0,1,17.5,17.31ZM12,0A12,12,0,1,0,24,12,12,12,0,0,0,12,0Z" style="fill:#fff"/></svg>&nbsp'
+var tokenTeste = 'BQBF6dYa0Whz98JAlBg9rmfYWxw17aYkXhp5rTLsH_NdEXDSJtQB_WY-q6g2RiitwMmNdDhw4_F9dMIxzNE';
 
 
-const pegaToken2 = async () => {
-    const response = await fetch('https://accounts.spotify.com/api/token', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
-        },
-        body: {
-            'grant_type': 'client_credentials'
-        },
-    });
-    const meuJson = await response.json(); 
-    console.log(meuJson);
+var pegaToken = (function () {
+    fetch('https://accounts.spotify.com/api/token', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Authorization": "Basic " + btoa(clientId + ':' + clientSecret)
+            },
+            body: 'grant_type=client_credentials'
+        })
+        .then(dataToken => {
+            return dataToken.json()
+        })
+        .then(token => {
+            console.log('sucesso token :', token.access_token);
+            novoToken = token.access_token
+            spotifyApi.setAccessToken(novoToken);
+            return novoToken
+        })
+        .catch(error => console.log(error));
 
-}
 
+})();
 
-var pegaToken = {
-    "url": "https://accounts.spotify.com/api/token",
-    "method": "POST",
-    "headers": {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
+spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE').then(
+    function (data) {
+        console.log('Artist albums', data);
     },
-    "data": {
-        "grant_type": "client_credentials"
-    },
-    success:function (sucesso){
-    console.log('suesso', sucesso.access_token)
-    token = sucesso.access_token;
-    return token
+    function (err) {
+        console.error(err);
     }
-};
+);
 
-
-$.ajax(pegaToken).done({
-    type:"GET",
-    url: 'https://api.spotify.com/v1/artists/0TnOYISbd1XYRBk9myaseg',
-    headers: {
-        'Authorization': 'Bearer ' + token,
+spotifyApi.getArtists(['2hazSY4Ef3aB9ATXW7F5w3', '6J6yx1t3nwIDyPXk5xa7O8']).then(
+    function (data) {
+        console.log('Artists information', data);
     },
-    sucess: function(){
-                //gazolla
-                var tabelaGaz = document.getElementById("nome-gazolla");
-                var fotoGaz = document.getElementById("foto-gazolla");
-                var seguidoresGaz = document.getElementById("seguidores-gazolla");
-                tabelaGaz.innerHTML = data.artists[0].name;
-                fotoGaz.innerHTML = '<img src="' + data.artists[0].images[2].url + '" class="img-tabela" alt="" srcset=""/>';
-                seguidoresGaz.innerHTML = spotifyIcon + data.artists[0].followers.total + '&nbspSeguidores';
+    function (err) {
+        console.error(err);
     }
-    
-
-});
-
-
-//var apenas para teste - token real
-var meuToken = 'BQDTgYVVgLHpfN6ngDG4ITpRnWJqetbV4ecgDdZzFLdIJg6ulPH_Hq-SZvH2h3_48W6lkJD60LjAG1GHLUg'
+);
 
 $.ajax({
     type: "GET",
     url: 'https://api.spotify.com/v1/artists?ids=6MXMuAa4reykXVUPFaSE2q%2C3yujwOo5L5DZDOcGj8K9fj%2C1VD9v9T9bOrZXhfZ8ZAHK9%2C5IRGhffWFbNGJqS7wc7UDN%2C3Gmuv8ih6UWYlZQlM4zFgo%2C66DRc0RcwzeQcm1wKdDQ4o%2C4ESBMlhP8DUP0k5mIJ2xfJ%2C6QmiUgayhWQeUFTRtBzMT8%2C29fIOE8ckNq96NbZlktZ7a%2C5rOuAFH0SjUPYfw8czrNaP%2C7qd3q5BKu3lh5r0vVBWSzm%2C7aS93G9e7XG0GCUATQjqv0%2C3anHEyEBxuE2hsDhHVlzZi%2C01Vnbpcaztlxks2JFHYkME%2C1BkGeGrbRHYd8Wg2lQqJqR%2C4xMfBjNDW6VPe7W4Wly5W7',
     async: 'false',
     headers: {
-        'Authorization': 'Bearer ' + meuToken,
+        'Authorization': 'Bearer ' + tokenTeste,
     },
     success: function (data) {
 
-        
+
 
         //gazolla
         var tabelaGaz = document.getElementById("nome-gazolla");
